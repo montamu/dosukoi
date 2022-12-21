2d9d6d941486a6d395d4852662bf7e3381ac05f7
montamu <kimremon0611@gmail.com>
2022-12-21 15:56:11 +0900

    react routerの初期設定をして、トップページのルーティング設定を完了しました

---
+++ src/components/appBar.tsx
@@ -0,0 +1,12 @@
+const AppBar = () => {
+  return (
+    <div>
+      <p>どすこい</p>
+      <p>このサイトについて</p>
+      <p>ログイン</p>
+      <p>新規登録</p>
+    </div>
+  )
+}
+
+export default AppBar;
---
+++ src/routes/home.tsx
@@ -0,0 +1,11 @@
+import AppBar from '../components/appBar'
+
+const Home = () => {
+  return (
+    <>
+      <AppBar />   
+    </>
+  );
+}
+
+export default Home;
